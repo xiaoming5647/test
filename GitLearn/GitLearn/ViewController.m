800bcaa1de8873e39b8b1efda51637758e999531
Wang Xiaoming <xiaoming.5647@163.com>
2018-01-29 15:03:40 +0800

    add a project

---
+++ GitLearn/GitLearn/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitLearn
+//
+//  Created by 晓明 on 2018/1/29.
+//  Copyright © 2018年 晓明. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ GitLearn/GitLearn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitLearn
+//
+//  Created by 晓明 on 2018/1/29.
+//  Copyright © 2018年 晓明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
